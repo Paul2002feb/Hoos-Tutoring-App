4a1419a353bc2485c627a77e8c60553e7b871895
zbelkhay <114372004+zbelky@users.noreply.github.com>
2023-02-23 13:00:53 -0500

    set new home links

--- home/urls.py
+++ home/urls.py
@@ -4,5 +4,5 @@ from . import views
 
 app_name = 'home'
 urlpatterns = [
-    path('', views.home, name='index'),
+    path('', views.IndexView.as_view(), name='index'),
 ]
--- home/views.py
+++ home/views.py
@@ -1,6 +1,14 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
+from django.template import loader
+from django.http import Http404
+from django.shortcuts import get_object_or_404, render
+from django.urls import reverse
+from .models import Choice, Question
+from django.views import generic
 
 # Create your views here.
 def home(request):
     return HttpResponse("Hello, world.")
+
+class IndexView(generic.ListView):
+    template_name = 'home/index.html'
