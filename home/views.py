ce1573556fdd3430c29d4a7ce174251125f305f5
zbelkhay <114372004+zbelky@users.noreply.github.com>
2023-02-23 13:07:02 -0500

    fixed imports in views

--- home/views.py
+++ home/views.py
@@ -3,7 +3,6 @@ from django.template import loader
 from django.http import Http404
 from django.shortcuts import get_object_or_404, render
 from django.urls import reverse
-from .models import Choice, Question
 from django.views import generic
 
 # Create your views here.
