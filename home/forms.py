f3291a1b63bea541621bc001731474e03c0367da
zbelkhay <114372004+zbelky@users.noreply.github.com>
2023-02-26 17:54:56 -0500

    fix again

--- home/forms.py
+++ home/forms.py
@@ -3,16 +3,16 @@ from allauth.account.forms import SignupForm
 from allauth.socialaccount.forms import SignupForm as SocialSignup
 from django import forms
 
-# class TutoringSignupForm(SignupForm):
-#     is_tutor = forms.BooleanField()
-#     def signup(self, request, user):
-#         user = super(TutoringSignupForm, self).save(request)
-#         tutoring_user = TutoringUser(
-#             user=user,
-#             is_tutor=self.is_tutor,
-#         )
-#         tutoring_user.save()
-#         return tutoring_user.user
+class TutoringSignupForm(SignupForm):
+    is_tutor = forms.BooleanField()
+    def signup(self, request, user):
+        user = super(TutoringSignupForm, self).save(request)
+        tutoring_user = TutoringUser(
+            user=user,
+            is_tutor=self.is_tutor,
+        )
+        tutoring_user.save()
+        return tutoring_user.user
 
 class TutoringSocialSignupForm(SocialSignup):
     is_tutor = forms.BooleanField()
