544a513f2e29ada4f3f5edbc1a1177c5c0938a9b
zbelky <114372004+zbelky@users.noreply.github.com>
2023-03-19 14:45:41 -0400

    added classes field for tutor user and request fields

--- home/models.py
+++ home/models.py
@@ -1,6 +1,24 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.contrib.postgres.fields import ArrayField
 
 class TutoringUser(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     is_tutor = models.BooleanField(default=False)
+    full_name = models.CharField(max_length=50)
+    MAJOR_CHOICES = [
+        ('ECE', 'Electrical/Computer Engineering'),
+        ('BME', 'Biomedical Engineering'),
+        ('CS', 'Computer Science'),
+        ('HIS', 'History'),
+        ('PSY', 'Psychology'),
+        ('PSY', 'Psychology')
+    ]
+    major = models.CharField(max_length=3, choices=MAJOR_CHOICES)
+    classes = ArrayField(models.CharField(max_length=20))
+
+class TutorRequest(models.Model):
+    is_verified = models.BooleanField(default=False)
+    request_user = models.CharField(max_length=50)
+    request_tutor = models.CharField(max_length=50)
+    request_time = models.DateTimeField()
