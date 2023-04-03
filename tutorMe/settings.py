2394e182628015c0d5b63754bd20ffb87c6e16b5
zbelkhay <114372004+zbelky@users.noreply.github.com>
2023-04-03 19:08:01 -0400

    fix login again

--- tutorMe/settings.py
+++ tutorMe/settings.py
@@ -152,7 +152,7 @@ AUTHENTICATION_BACKENDS = [
     'allauth.account.auth_backends.AuthenticationBackend'
 ]
 
-SITE_ID = 1
+SITE_ID = 2
 LOGIN_REDIRECT_URL = '/'
 
 SOCIALACCOUNT_QUERY_EMAIL = True
